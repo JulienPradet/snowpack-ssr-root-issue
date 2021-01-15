5306ceca521f40347c569c8c7cc33c2aad447792
Julien Pradet <julien.pradet@gmail.com>
2021-01-15 17:45:43 +0100

    Initial commit

---
+++ cli/server.js
@@ -0,0 +1,22 @@
+const { join } = require("path");
+const { startServer, loadConfiguration } = require("snowpack");
+
+const run = async () => {
+  const snowpackConfig = await loadConfiguration(
+    {
+      root: join(__dirname, "../client"),
+    },
+    join(__dirname, "./snowpack.config.json")
+  );
+
+  const server = await startServer({
+    config: snowpackConfig,
+    lockfile: null,
+  });
+  const runtime = server.getServerRuntime();
+
+  const module = await runtime.importModule("/example.js");
+  console.log(module.exports.default());
+};
+
+run();
